ce8ca2da6dfbf982f4914b0c96968d9c92cc2d12
Saleh Zaidan <salehzaidan64@gmail.com>
2022-12-03 12:02:28 +0700

    Create hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <section>
-      <h1>Hi there, I&apos;m Zaidan</h1>
-      <p>IoT, AI, and Web Dev enthusiast</p>
+    <section className="flex flex-col items-center gap-4 py-4 sm:flex-row sm:justify-center sm:gap-8">
+      <Image
+        src="/avatar.jpeg"
+        alt="Saleh Zaidan"
+        width={128}
+        height={128}
+        className="rounded-full"
+      />
+      <div className="text-center sm:text-left">
+        <h1 className="text-3xl font-medium sm:text-4xl">
+          Hi there, I&apos;m Zaidan
+        </h1>
+        <p className="text-lg sm:text-xl">IoT, AI, and Web Dev enthusiast</p>
+      </div>
     </section>
   );
 }
