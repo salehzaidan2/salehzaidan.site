c395e07d1aa6c77234d2835cd59b28902a2709da
Saleh Zaidan <salehzaidan64@gmail.com>
2022-12-10 08:40:44 +0700

    Modify focus ring

--- components/work-card.tsx
+++ components/work-card.tsx
@@ -16,33 +16,34 @@ export function WorkCard({
   thumbnail,
 }: WorkCardProps) {
   return (
-    <article className="group relative w-[18.25rem] space-y-2 sm:space-y-0">
-      <div className="relative aspect-video">
-        <Image
-          src={`/static/images/${slug}/${thumbnail.url}`}
-          alt={thumbnail.name}
-          fill
-          className="rounded-md object-cover object-top shadow-md"
-        />
-      </div>
-      <div className="cursor-pointer rounded-b-md text-center transition-opacity duration-300 group-hover:opacity-100 sm:absolute sm:bottom-0 sm:bg-black/75 sm:px-4 sm:py-2 sm:text-white sm:opacity-0">
-        <h3 className="text-lg font-medium">{name}</h3>
-        <p
-          className="overflow-hidden text-sm leading-tight"
-          style={{
-            display: "-webkit-box",
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: "vertical",
-          }}
-        >
-          {description}
-        </p>
-      </div>
+    <article className="group relative w-[18.25rem]">
       <Link
         href={`/works/${slug}`}
         aria-label={name}
-        className="after:absolute after:inset-0"
-      ></Link>
+        className="block space-y-2 rounded-md sm:space-y-0"
+      >
+        <div className="relative aspect-video">
+          <Image
+            src={`/static/images/${slug}/${thumbnail.url}`}
+            alt={thumbnail.name}
+            fill
+            className="rounded-md object-cover object-top shadow-md"
+          />
+        </div>
+        <div className="cursor-pointer rounded-b-md text-center transition-opacity duration-300 group-hover:opacity-100 sm:absolute sm:bottom-0 sm:bg-black/75 sm:px-4 sm:py-2 sm:text-white sm:opacity-0">
+          <h3 className="text-lg font-medium">{name}</h3>
+          <p
+            className="overflow-hidden text-sm leading-tight"
+            style={{
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+            }}
+          >
+            {description}
+          </p>
+        </div>
+      </Link>
     </article>
   );
 }
